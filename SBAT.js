2f994e34662677dc66f3af78c11b168779b999ec
Jia Sheng <shengjia1100@gmail.com>
2024-06-10 16:21:33 +0200

    fix upload and download content

--- SBAT.js
+++ SBAT.js
@@ -19,7 +19,7 @@ let shortcutList;
 let multilabel = false;
 let saveToLocalStorage = false;
 let keyCodeList = { "0": 48, "1": 49, "2": 50, "3": 51, "4": 52, "5": 53, "6": 54, "7": 55, "8": 56, "9": 57, "d": 68, "b": 66, "a": 65, "s": 83, "i": 73, "f": 70, "k": 75, "ß": 219, "Dead": 220, "+": 187, "ü": 186, "p": 80, "o": 79, "u": 85, "z": 90, "t": 84, "r": 82, "e": 69, "w": 87, "g": 71, "h": 72, "j": 74, "l": 76, "ö": 192, "ä": 222, "#": 191, "y": 89, "x": 88, "c": 67, "v": 86, "n": 78, "m": 77, ",": 188, ".": 190, "-": 189, "ArrowRight": 39, "ArrowLeft": 37, "ArrowUp": 38, "ArrowDown": 40, "PageDown": 34, "Clear": 12, "Home": 36, "PageUp": 33, "End": 35, "Delete": 46, "Insert": 45, "Control": 17, "AltGraph": 18, "Meta": 92, "Alt": 18, "Shift": 16, "CapsLock": 20, "Tab": 9, "Escape": 27, "F1": 112, "F2": 113, ";": 188, ":": 190, "_": 189, "'": 191, "*": 187, "Q": 81, "W": 87, "E": 69, "R": 82, "T": 84, "Z": 90, "S": 83, "A": 65, "D": 68, "I": 73, "U": 85, "O": 79, "Y": 89, "X": 88, "C": 67, "F": 70, "V": 86, "G": 71, "B": 66, "H": 72, "N": 78, "J": 74, "M": 77, "K": 75, "L": 76, "P": 80, "Ö": 192, "Ä": 222, "Ü": 186, "!": 49, "\"": 50, "§": 51, "$": 52, "%": 53, "&": 54, "/": 55, "(": 56, ")": 57, "=": 48, "?": 219, "°": 220 }
-let authenticatedUser = '';
+//let authenticatedUser = '';
 
 // HTML Elements
 let textDisplay = document.getElementById('textDisplay');
@@ -192,7 +192,9 @@ function loadJson(data) {
     enteredLabelSet.value = '';
     for (let i = 0; i < parsedJson.data.length; i++) {
         inputData.push(parsedJson.data[i].text);
-        outputData.push(parsedJson.data[i]);
+        //let mappedData = { ...parsedJson.data[i], spans: new Map(Object.entries(parsedJson.data[i].spans))}
+        outputData.push({ ...parsedJson.data[i], spans: new Map(Object.entries(parsedJson.data[i].spans)) });
+        console.log(parsedJson.data[i])
     }
     if (parsedJson.labelSet.length > 0) {
         for (let i = 0; i < parsedJson.labelSet.length; i++) {
@@ -679,15 +681,17 @@ function downloadButtonClicked() {
 
     console.log("downloadButtonClicked")
 
-    let cleanedOutputData = outputData;
+    let cleanedOutputData = outputData.map(
+        (data) => { return { ...data, spans: Object.fromEntries(data.spans) } })
+    //let cleanedOutputData = [];
+
     /*
-    for (let i = 0; i < outputData.length; i++){
-        for (let j = 0; j < outputData[i].label.length; j++){
+    for (let i = 0; i < outputData.length; i++) {
+        cleanedOutputData[i] = outputData[i].labels
+        for (let j = 0; j < outputData[i].label.length; j++) {
             cleanedOutputData[i].label[j] = outputData[i].label[j].substring(getSubLabelLevel(outputData[i].label[j]));
         }
-        
-    }
-    */
+    }*/
 
     let dataToWrite = new Object();
     dataToWrite.labelSet = labelSet;
