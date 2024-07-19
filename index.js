f6375576713533a69780f27b729c4b121f83ca7e
Jia Sheng <shengjia1100@gmail.com>
2024-07-19 18:35:20 +0200

    fix load file from github

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 import { BratFrontendEditor } from "./Brat.js"
+import { Octokit } from "https://esm.sh/octokit@2.1.0";
 
 
 let fileSelector = document.getElementById('fileSelector');
 let downloadButton = document.getElementById('downloadButton');
 let downloadArea = document.getElementById('downloadArea');
+let patArea = document.getElementById('patArea')
+let repoSelectArea = document.getElementById('repoSelectArea')
+let fileSelectArea = document.getElementById('fileSelectArea')
+let personalAccessToken = document.getElementById('personalAccessToken');
+let authenticationButton = document.getElementById('authenticationButton');
+let repoSelect = document.getElementById('repoSelect');
+let repoSelectButton = document.getElementById('repoSelectButton');
+let fileSelect = document.getElementById('fileSelect');
+let fileSelectButton = document.getElementById('fileSelectButton');
+let commitPatArea = document.getElementById('commitPatArea')
+let commitPersonalAccessToken = document.getElementById('commitPersonalAccessToken')
+let commitAuthenticationButton = document.getElementById('commitAuthenticationButton');
+let fileCreateArea = document.getElementById('fileCreateArea')
+let commitFileName = document.getElementById('commitFileName')
+let commitMessageArea = document.getElementById('commitMessageArea')
+let commitMessage = document.getElementById('commitMessage')
+let commitConfirmButton = document.getElementById('commitConfirmButton')
+let octokit;
+let userName;
+let repos;
+let files;
+let repoName;
+let fileName;
+let text;
 let brat;
 
 downloadArea.hidden = true;
+repoSelectArea.hidden = true;
+fileSelectArea.hidden = true;
+fileCreateArea.hidden = true;
+commitMessageArea.hidden = true;
+
+
+authenticationButton.addEventListener('click', authenticationButtonClicked);
+commitAuthenticationButton.addEventListener('click', authenticationButtonClicked);
+repoSelectButton.addEventListener('click', getRepoFiles);
+fileSelectButton.addEventListener('click', getFileContent);
+downloadButton.addEventListener('click', downloadButtonClicked);
+fileSelector.addEventListener('change', (event) => {
+    getFileData(event.target.files[0]);
+    downloadArea.hidden = false;
+})
 
 let options = {
     assetsPath: "static/", webFontURLs: ['fonts/Astloch-Bold.ttf', 'fonts/PT_Sans-Caption-Web-Regular.ttf', 'fonts/Liberation_Sans-Regular.ttf'], ajax: 'local', overWriteModals: false, maxFragmentLength: 30, showTooltip: true
@@ -43,12 +83,6 @@ fetch('./config.json')
                 initializeBrat()
             }
         })
-
-downloadButton.addEventListener('click', downloadButtonClicked);
-fileSelector.addEventListener('change', (event) => {
-    getFileData(event.target.files[0]);
-    downloadArea.hidden = false;
-})
 
 function downloadButtonClicked() {
     console.log("downloadButtonClicked")
@@ -117,11 +151,18 @@ function loadTxt(textData) {
 
 function loadJson(data) {
     console.log("loadJson")
-    let json = data;
-    let parsedJson = JSON.parse(json);
-    docData = parsedJson.docData;
-    collData = parsedJson.collData;
-    updateBratEditor()
+    let parsedJson = JSON.parse(data);
+    if (parsedJson.docData && parsedJson.collData) {
+        docData = parsedJson.docData;
+        collData = parsedJson.collData;
+        brat.docData = docData;
+        brat.collData = collData;
+        updateBratEditor()
+    } else {
+        window.alert("Incompatible json format, file loaded as pure string")
+        loadTxt(data)
+    }
+
 }
 
 function updateBratEditor() {
@@ -129,5 +170,119 @@ function updateBratEditor() {
         brat.dispatcher.post('collectionLoaded', [collData]);
         brat.dispatcher.post('requestRenderData', [docData]);
         brat.dispatcher.post('current', [collData, docData, {}]);
+    }
+}
+
+
+async function authenticationButtonClicked() {
+    console.log("authenticationButtonClicked")
+    let authKey = personalAccessToken.value;
+    octokit = new Octokit({ auth: authKey });
+    getUserName()
+        .then(() => getUserRepos())
+    patArea.hidden = true;
+    repoSelectArea.hidden = false;
+    commitPatArea.hidden = true;
+
+}
+
+const getUserName = async () => {
+    try {
+        const {
+            data: { login },
+        } = await octokit.rest.users.getAuthenticated();
+        userName = login;
+        console.log("Hello, %s", login);
+    } catch (error) {
+        console.error('Error fetching username:', error)
+    }
+
+}
+
+const getUserRepos = async () => {
+    try {
+        const response = await octokit.rest.repos.listForUser({
+            username: userName,
+            type: "owner",
+            sort: "full_name",
+            per_page: 100
+        })
+        repos = await response.data.map(repo => repo.name);
+        makeRepoDropdown()
+        console.log(repos)
+    } catch (error) {
+        console.error('Error fetching repo list:', error)
+    }
+}
+
+const makeRepoDropdown = () => {
+    repoSelect.innerHTML = '';
+    repos.forEach(repo => {
+        const option = document.createElement('option');
+        option.value = repo;
+        option.textContent = repo;
+        repoSelect.appendChild(option);
+    });
+}
+
+const makeFileDropdown = () => {
+    fileSelect.innerHTML = '';
+    files.forEach(file => {
+        const option = document.createElement('option');
+        option.value = file;
+        option.textContent = file;
+        fileSelect.appendChild(option);
+    });
+}
+
+async function getRepoFiles() {
+    try {
+        repoName = repoSelect.value;
+        const response = await octokit.rest.repos.getContent({
+            owner: userName,
+            repo: repoName,
+            path: '',
+        });
+        files = await response.data.map(file => file.path);
+        makeFileDropdown()
+        repoSelectArea.hidden = true;
+        fileSelectArea.hidden = false;
+    } catch (error) {
+        console.error('Error fetching file list:', error)
+    }
+}
+
+async function getFileContent() {
+    try {
+        fileName = fileSelect.value;
+        const response = await octokit.rest.repos.getContent({
+            owner: userName,
+            repo: repoName,
+            path: fileName,
+        });
+        if (response.data.content) {
+            text = atob(response.data.content);
+            if (extractJSON(text)) {
+                loadJson(text)
+            } else {
+                loadTxt(text)
+            }
+        }
+        fileSelectArea.hidden = true;
+        patArea.value = ""
+        patArea.hidden = false;
+        commitPatArea.hidden = false;
+        downloadArea.hidden = false;
+    } catch (error) {
+        console.error('Error fetching file content:', error)
+    }
+}
+
+const extractJSON = (str) => {
+    try {
+        JSON.parse(str);
+        return true;
+    } catch (e) {
+        return false;
     }
 }
